797bb2e99baac234e8e94cf992dc0599e7357a7f
omer-altan <s212512@dtu.dk>
2022-04-07 11:05:28 +0200

    The std mean,meansubt,energy is added into mainactivity

--- app/src/main/java/com/example/wappnea/MainActivity.java
+++ app/src/main/java/com/example/wappnea/MainActivity.java
@@ -29,5 +29,51 @@ public class MainActivity extends AppCompatActivity {
         Intent intent_1 = new Intent(this, WhileSleeping.class);
         startActivity(intent_1);
     }
+
+    // reference https://searchcode.com/codesearch/view/78094523/
+    private static double mean(double[] datawindow){
+
+        double result =0.0;
+        for (int i=0;i<datawindow.length;i++){
+            result += datawindow[i];
+        }
+
+        result /= (double)datawindow.length;
+
+        return result;
+    }
+    //https://www.geeksforgeeks.org/java-program-to-calculate-standard-deviation/
+    private static double std(double[] datawindow){
+        double standardDeviation = 0.0;
+        double meanresult =mean(datawindow);
+        for (int i = 0; i < datawindow.length; i++) {
+            standardDeviation = standardDeviation + Math.pow((datawindow[i] - meanresult), 2);
+        }
+        double res = Math.sqrt(standardDeviation / datawindow.length);
+        return res;
+    }
+
+    private static double energy(double[] datawindow){
+        double energy = 0.0;
+        for (int i = 0; i < datawindow.length; i++) {
+            energy=energy+(datawindow[i])*(datawindow[i]);
+        }
+        energy /= (double)datawindow.length;
+
+        return energy;
+    }
+
+    // reference https://searchcode.com/codesearch/view/78094523/
+    private static double[] substractMean(double[] datawindow){
+
+        double[] substracted = new double[datawindow.length];
+        double meanresult =mean(datawindow);
+
+        for (int i=0;i<datawindow.length;i++){
+            substracted[i] = datawindow[i] - meanresult;
+        }
+        return substracted;
+    }
 }
+
 
