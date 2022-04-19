65c0d7e7aaef8b03f42131c853087cf1ec86e8e4
omer-altan <s212512@dtu.dk>
2022-04-19 17:14:54 +0200

    The std mean,meansubt,energy is added into mainactivity

--- app/src/main/java/com/example/wappnea/MainActivity.java
+++ app/src/main/java/com/example/wappnea/MainActivity.java
@@ -31,48 +31,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // reference https://searchcode.com/codesearch/view/78094523/
-    private static double mean(double[] datawindow){
+    private static double mean(double[] dataWindow){
 
         double result =0.0;
-        for (int i=0;i<datawindow.length;i++){
-            result += datawindow[i];
+        for (int i=0;i<dataWindow.length;i++){
+            result += dataWindow[i];
         }
 
-        result /= (double)datawindow.length;
+        result /= (double)dataWindow.length;
 
         return result;
     }
     //https://www.geeksforgeeks.org/java-program-to-calculate-standard-deviation/
-    private static double std(double[] datawindow){
+    private static double std(double[] dataWindow){
         double standardDeviation = 0.0;
-        double meanresult =mean(datawindow);
-        for (int i = 0; i < datawindow.length; i++) {
-            standardDeviation = standardDeviation + Math.pow((datawindow[i] - meanresult), 2);
+        double meanResult =mean(dataWindow);
+        for (int i = 0; i < dataWindow.length; i++) {
+            standardDeviation = standardDeviation + Math.pow((dataWindow[i] - meanResult), 2);
         }
-        double res = Math.sqrt(standardDeviation / datawindow.length);
+        double res = Math.sqrt(standardDeviation / dataWindow.length);
         return res;
     }
 
-    private static double energy(double[] datawindow){
+    private static double energy(double[] dataWindow){
         double energy = 0.0;
-        for (int i = 0; i < datawindow.length; i++) {
-            energy=energy+(datawindow[i])*(datawindow[i]);
+        for (int i = 0; i < dataWindow.length; i++) {
+            energy=energy+(dataWindow[i])*(dataWindow[i]);
         }
-        energy /= (double)datawindow.length;
+        energy /= (double)dataWindow.length;
 
         return energy;
     }
 
     // reference https://searchcode.com/codesearch/view/78094523/
-    private static double[] substractMean(double[] datawindow){
+    private static double[] subMean(double[] dataWindow){
 
-        double[] substracted = new double[datawindow.length];
-        double meanresult =mean(datawindow);
+        double[] subst = new double[dataWindow.length];
+        double meanResult =mean(dataWindow);
 
-        for (int i=0;i<datawindow.length;i++){
-            substracted[i] = datawindow[i] - meanresult;
+        for (int i=0;i<dataWindow.length;i++){
+            subst[i] = dataWindow[i] - meanResult;
         }
-        return substracted;
+        return subst;
     }
 }
 
