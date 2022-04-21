4641bdcd1b7b6195f43b31389cf35e8e7b0b5684
agn123 <agnese.calvani@gmail.com>
2022-04-21 10:57:53 +0200

    standardization method added to main

--- app/src/main/java/com/example/wappnea/MainActivity.java
+++ app/src/main/java/com/example/wappnea/MainActivity.java
@@ -86,6 +86,17 @@ public class MainActivity extends AppCompatActivity {
         }
         return subst;
     }
+
+    private static double[] standardization(double[] parameterValues){
+        double meanParam = mean(parameterValues);
+        double stdParam = std(parameterValues);
+        double[] standardizedParameterValues = new double[parameterValues.length];
+
+        for (int i=0; i<parameterValues.length; i++){
+            standardizedParameterValues[i] = (parameterValues[i]-meanParam)/stdParam;
+        }
+        return standardizedParameterValues;
+    }
 }
 
 
